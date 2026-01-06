c6fa51be057f691ad71edfddf03dee4b1b78c6ef
Karen Liseth <klmendez@unimayor.edu.co>
2026-01-05 19:14:01 -0500

    pasar la variable Deploy al build

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,18 @@
 import type { NextConfig } from "next";
 
+const isGithubPages = process.env.DEPLOY_TARGET === "github";
+
 const nextConfig: NextConfig = {
-  /* config options here */
   reactCompiler: true,
+
+  // Solo necesario para GitHub Pages (sitio estático)
+  ...(isGithubPages
+    ? {
+        output: "export",
+        basePath: "/karen-portfolio",
+        images: { unoptimized: true },
+      }
+    : {}),
 };
 
 export default nextConfig;
